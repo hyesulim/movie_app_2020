ffa51af32db9749a76091b8d6d36f9620f25967e
limhyesu98 <redbaloon17@gmail.com>
2020-05-12 21:34:42 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -4,24 +4,19 @@ import PropTypes from 'prop-types'; // you can check data type and requiredness
 // react automatically is going to execute render method of your class component
 
 class App extends React.Component{
-  state = { // we want to change the data in our component
-    count: 0
+  state = {
+    isLoading : true
+  };
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading: false});
+    }, 6000);
   }
-  // everytime you call setState, react will call the render function with the new state
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  }
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
-  }
-  // onClick={this.add} => when you click, run add
-  // this.add() => run add immediately
   render(){
+    const {isLoading} = this.state;
     return <div>
-      <h1>The number is {this.state.count}</h1> 
-      <button onClick={this.add}>Add</button> 
-      <button onClick={this.minus}>Minus</button>
-    </div>
+      {isLoading ? "Loading...": "We are ready"}
+    </div>;
   }
 }
 
