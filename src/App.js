22f476c63f4637802205e3f3eb1f1e11e5597fb3
limhyesu98 <redbaloon17@gmail.com>
2020-05-12 21:22:31 +0900

    #3.1 All you need to know about State

--- src/App.js
+++ src/App.js
@@ -1,71 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // you can check data type and requiredness
 
-function Food({fav, picture, rating}){
-  //console.log(props.fav);
-  return <div>
-    <h2>I line {fav}</h2>
-    <h3>{rating}/5.0</h3>
-    <img src={picture} alt={fav}></img>
-  </div>
-}
+// react automatically is going to execute render method of your class component
 
-Food.propTypes = { // it has to be named "propTypes"
-  fav: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-  picture: PropTypes.string.isRequired
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 4.3
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.2
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 4.2
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 3.9
+class App extends React.Component{
+  state = { // we want to change the data in our component
+    count: 0
   }
-];
-
-
-
-function App() {
-  return <div>
-    {foodILike.map(dish => (
-      <Food 
-      key={dish.id} 
-      fav={dish.name} 
-      picture={dish.image} 
-      rating={dish.rating}/>
-    ))}
-  </div>;
+  // everytime you call setState, react will call the render function with the new state
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  }
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  }
+  // onClick={this.add} => when you click, run add
+  // this.add() => run add immediately
+  render(){
+    return <div>
+      <h1>The number is {this.state.count}</h1> 
+      <button onClick={this.add}>Add</button> 
+      <button onClick={this.minus}>Minus</button>
+    </div>
+  }
 }
 
 export default App;
